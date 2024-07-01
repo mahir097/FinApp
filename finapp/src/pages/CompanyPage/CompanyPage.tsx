eb11fe8fd7acf06d85c68c8dc183f2624235e014
Mahir <mahir.demir097@gmail.com>
2024-07-01 14:33:35 +0300

    added dashboard

--- finapp/src/pages/CompanyPage/CompanyPage.tsx
+++ finapp/src/pages/CompanyPage/CompanyPage.tsx
@@ -2,13 +2,42 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { CompanyProfile } from "../../company";
 import { getCompanyProfile } from "../../api";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import CompanyDashboard from "../../components/CompanyDashboard/CompanyDashboard";
+import Tile from "../../components/Tile/Tile";
 
 interface Props {}
 
 const CompanyPage = (props: Props) => {
-  return;
   let { ticker } = useParams();
+  const tabItems = [
+    {
+      id: 1,
+      title: "Company Profile",
+      icon: "fas fa-child",
+      content: "step 1 content",
+    },
+    {
+      id: 2,
+      title: "Income Statment",
+      icon: "fas fa-users",
+      content: "step 2 content",
+    },
+    {
+      id: 3,
+      title: "Balance Sheet",
+      icon: "fas fa-network-wired",
+      content: "step 3 content",
+    },
+    {
+      id: 4,
+      title: "Cash Flow",
+      icon: "fa money-check-alt",
+      content: "step 4 content",
+    },
+  ];
   const [company, setCompany] = useState<CompanyProfile>();
+  const [activeSidebarItem, setActiveSideBarItem] = useState<number>(1);
 
   useEffect(() => {
     const getProfileInit = async () => {
@@ -21,7 +50,22 @@ const CompanyPage = (props: Props) => {
   return (
     <>
       {company ? (
-        <div className="company-profile-container">{company.companyName}</div>
+        <div className="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+          <Sidebar
+            tabItems={tabItems}
+            setActiveSideBarItem={setActiveSideBarItem}
+            activeSidebarItem={activeSidebarItem!}
+          />
+          <CompanyDashboard
+            tabItems={tabItems}
+            activeSidebarItem={activeSidebarItem!}
+          >
+            <Tile title="Company Name" subTitle={company.companyName} />
+            <Tile title="DCF" subTitle={company.dcf.toString()} />
+            <Tile title="Dividend" subTitle={company.lastDiv.toString()} />
+            <Tile title="Price" subTitle={company.price.toString()} />
+          </CompanyDashboard>
+        </div>
       ) : (
         <div>Company Not Found!</div>
       )}
