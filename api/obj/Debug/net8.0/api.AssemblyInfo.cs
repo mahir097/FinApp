b101952ddd7abc782af07bbb4ceec50c5b3701ac
Mahir <mahir.demir097@gmail.com>
2024-07-25 14:54:20 +0300

    Added Async/Await

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668ce74047484bd5a63a00f323e6ac87b63ff176")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8a9443d756b704bcb821654daa68715ac417b7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
