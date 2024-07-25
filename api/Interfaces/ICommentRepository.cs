0459d620fc6342c5a0f3d3cf2f31de5025cc95ef
Mahir <mahir.demir097@gmail.com>
2024-07-25 22:53:19 +0300

    Comment Put Method

--- api/Interfaces/ICommentRepository.cs
+++ api/Interfaces/ICommentRepository.cs
@@ -11,6 +11,8 @@ namespace api.Interfaces
         Task<List<Comment>> GetAllAsync();
         Task<Comment?> GetByIdAsync(int id);
         Task<Comment> CreateAsync(Comment commentModel);
+        Task<Comment?> UpdateAsync(int id, Comment commentModel);
+
 
     }
 }
