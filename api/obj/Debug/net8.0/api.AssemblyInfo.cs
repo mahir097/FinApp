9296de45cad8d23caf26d7c1d1754a7911e89b01
Mahir <mahir.demir097@gmail.com>
2024-07-25 22:10:47 +0300

    One to Many Post Method

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d1b4c2c3e367c183a434a7aa9ac365add3211e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+796ce02a482174aa99f82eebf818badd9f7857ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
