e8a9443d756b704bcb821654daa68715ac417b7e
Mahir <mahir.demir097@gmail.com>
2024-07-25 14:43:43 +0300

    Added Delete Method

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f527a6b350718c050a06ed57863767308f79b1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+668ce74047484bd5a63a00f323e6ac87b63ff176")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
