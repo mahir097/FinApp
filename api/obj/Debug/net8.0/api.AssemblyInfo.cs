e15e09caf72a6c47bea85e145a92b427f0935694
Mahir <mahir.demir097@gmail.com>
2024-07-26 23:50:23 +0300

    Portfolio Get Method

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8ced5db62d70ce6933080aedd7b322eafb9887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ee4920745d89886afdd8251d4a75b545b100fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
