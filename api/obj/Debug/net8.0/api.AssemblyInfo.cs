c7831e53700164c7e09046aeef0eb19612d0b5e0
Mahir <mahir.demir097@gmail.com>
2024-07-27 14:16:16 +0300

    Generated content from user

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8ee4920745d89886afdd8251d4a75b545b100fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cf6af2def61be0bb168b3c3eb1e4252cbd29959")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
