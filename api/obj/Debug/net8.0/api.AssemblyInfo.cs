924e081ddd0ec9973d4af6b78bac8aac82fcdf96
Mahir <mahir.demir097@gmail.com>
2024-07-28 17:22:41 +0300

    Added Logout page

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce59831f35d1edddf06aed85b391b38eff623ce7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e0bc361de755ed7af2937178b52d5cba9d236b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
